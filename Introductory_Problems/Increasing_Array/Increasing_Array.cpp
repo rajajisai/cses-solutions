d85d464c0ae5b90705e431abd11e2289cade657b
rajajisai <40017333+rajajisai@users.noreply.github.com>
2026-05-28 16:50:38 -0400

    Add C++ solution for Increasing_Array

---
+++ Introductory_Problems/Increasing_Array/Increasing_Array.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Increasing_Array
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-05-28
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:12:22: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   12 |         for(int i=1;i<arr.size();i++){
+      |                     ~^~~~~~~~~~~
